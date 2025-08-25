38805966210a1e481f9f3fd997181b9ff077fecb
Abhishek Pandey <abhi.ecc.cs@gmail.com>
2025-08-25 17:54:03 +0530

    adding the ctegory menu and top doctors list

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../component/Header'
 import SpecialityMenu from '../component/SpecialityMenu'
+import TopDoctor from '../component/TopDoctor'
 
 function Home() {
   return (
     <div>
       <Header/>
       <SpecialityMenu/>
+      <TopDoctor/>
     </div>
   )
 }
